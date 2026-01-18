53d6d0d1fb0bdc5cef887d9429096db42f213243
Neevithaa <neevithaa@gmail.com>
2026-01-18 15:14:54 +0530

    Add files via upload

---
+++ src/components/Weather.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
+
+function Weather() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState("");
+
+  const API_KEY = "a2015c19ebb2065aa2b530add87e4878";
+
+  const getWeather = async () => {
+    if (!city) {
+      setError("Please enter a city name");
+      setWeather(null);
+      return;
+    }
+
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      );
+     
+
+      setWeather(response.data);
+      setError("");
+    } catch (err) {
+      setError("City not found. Please try again.");
+      setWeather(null);
+    }
+  };
+
+  return (
+    <section className="weather-container">
+      <h1>🌦️ Weather Report</h1>
+
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={getWeather}>Search</button>
+      </div>
+
+      {error && <p className="error">{error}</p>}
+
+      {weather && (
+        <div className="weather-card">
+          <h2>{weather.name}</h2>
+          <p>🌡️ Temperature: {weather.main.temp} °C</p>
+          <p>💧 Humidity: {weather.main.humidity}%</p>
+          <p>🌬️ Wind Speed: {weather.wind.speed} m/s</p>
+          <p>☁️ Condition: {weather.weather[0].description}</p>
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default Weather;
